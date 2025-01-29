188b0f524af7f6c4f84b92b66b9957e6dbeefae2
Ong Song Yi <ong_song_yi@aiap.sg>
2025-01-29 19:28:02 +0800

    feat: added general utils which contain logging

---
+++ Game of Thrones Chatbot/src/utils/general_utils.py
@@ -0,0 +1,35 @@
+import logging
+import logging.config
+import os
+
+import yaml
+
+logger = logging.getLogger(__name__)
+
+
+def setup_logging(
+    logging_config_path="./conf/logging.yaml", default_level=logging.INFO
+):
+    """Set up configuration for logging utilities.
+
+    Parameters
+    ----------
+    logging_config_path : str, optional
+        Path to YAML file containing configuration for Python logger,
+        by default "./config/logging_config.yaml"
+    default_level : logging object, optional, by default logging.INFO
+
+    """
+    try:
+        os.makedirs("logs", exist_ok=True)
+        with open(logging_config_path, encoding="utf-8") as file:
+            log_config = yaml.safe_load(file.read())
+        logging.config.dictConfig(log_config)
+
+    except Exception as error:
+        logging.basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            level=default_level,
+        )
+        logger.info(error)
+        logger.info("Logging config file is not found. Basic config is used.")
