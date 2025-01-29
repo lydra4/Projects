3928c9d098d29cbb8136a107357292b570451d31
Ong Song Yi <ong_song_yi@aiap.sg>
2025-01-29 19:27:27 +0800

    feat: setting up training script

---
+++ Game of Thrones Chatbot/src/train.py
@@ -0,0 +1,32 @@
+import logging
+import os
+
+import hydra
+from langchain_community.document_loaders import DirectoryLoader
+from utils.general_utils import setup_logging
+
+
+@hydra.main(version_base=None, config_path="../conf", config_name="training.yaml")
+def main(cfg):
+    logger = logging.getLogger(__name__)
+    logger.info("Setting up logging configuration.")
+    setup_logging(
+        logging_config_path=os.path.join(
+            hydra.utils.get_original_cwd(), "conf", "logging.yaml"
+        )
+    )
+
+    epub_loader = DirectoryLoader(
+        path=cfg["preprocessing"]["path"],
+        glob=cfg["preprocessing"]["glob"],
+        show_progress=cfg["preprocessing"]["show_progress"],
+        use_multithreading=cfg["preprocessing"]["use_multithreading"],
+        sample_seed=cfg["preprocessing"]["sample_seed"],
+    )
+
+    logging.info("Loading Documents")
+    docs = epub_loader.load()
+
+
+if __name__ == "main":
+    main()
