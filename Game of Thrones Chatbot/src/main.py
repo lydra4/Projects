96b6a3bccd4dacbaeac533eb33bb2008255476ba
Ong Song Yi <ong_song_yi@aiap.sg>
2025-02-06 19:34:13 +0800

    feat: modularised embeddings code

--- Game of Thrones Chatbot/src/main.py
+++ Game of Thrones Chatbot/src/main.py
@@ -2,10 +2,7 @@ import logging
 import os
 
 import hydra
-import torch
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceInstructEmbeddings
-from langchain_community.vectorstores.faiss import FAISS
+from embeddings.perform_embeddings import PerformEmbeddings
 from utils.general_utils import setup_logging
 from utils.process_data import EPUBProcessor
 
@@ -23,47 +20,8 @@ def main(cfg):
     epub_processor = EPUBProcessor(cfg=cfg, logger=logger)
     documents = epub_processor.load()
 
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=cfg["embeddings"]["chunk_size"],
-        chunk_overlap=cfg["embeddings"]["chunk_overlap"],
-    )
-    texts = text_splitter.split_documents(documents)
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    try:
-        embeddings = HuggingFaceInstructEmbeddings(
-            model_name=cfg["embeddings"]["embeddings_model"],
-            model_kwargs={"device": device},
-        )
-        logger.info(f"Embedding Model loaded to {device.upper()}.")
-
-    except RuntimeError as e:
-        logger.error(f"CUDA memory issue: {e}. Switching to CPU.")
-        embeddings = HuggingFaceInstructEmbeddings(
-            model_name=cfg["embeddings"]["embeddings_model"],
-            model_kwargs={"device": "cpu"},
-        )
-
-    if not os.path.exists(cfg["embeddings"]["embeddings_path"]):
-        os.makedirs(name=cfg["embeddings"]["embeddings_path"], exist_ok=True)
-
-    logger.info("Generating Vector Embeddings")
-    vectordb = FAISS.from_documents(documents=texts, embedding=embeddings)
-    logger.info("Saving Vector Embeddings")
-    vectordb.save_local(
-        folder_path=cfg["embeddings"]["embeddings_path"],
-        index_name=cfg["embeddings"]["index_name"],
-    )
-
-    logging.info("Loading Vector DB")
-    vectordb = FAISS.load_local(
-        folder_path=cfg["embeddings"]["embeddings_path"],
-        index_name=cfg["embeddings"]["index_name"],
-        embeddings=embeddings,
-        allow_dangerous_deserialization=True,
-    )
-    logging.info("Successfully loaded")
+    perform_embeddings = PerformEmbeddings(cfg=cfg, logger=logger, documents=documents)
+    vector_db = perform_embeddings.process_and_load_vectordb()
 
 
 if __name__ == "__main__":
